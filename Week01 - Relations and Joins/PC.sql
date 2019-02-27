e4ea567c8c0bcbd9ad92f54ce1ab7892fc6f09eb
Kristiyan Gergov <kristiyan.gergov98@gmail.com>
2019-02-27 23:29:22 +0200

    Added task4 from week01 PC

--- Week01 - Relations and Joins/PC.sql
+++ Week01 - Relations and Joins/PC.sql
@@ -1,7 +1,7 @@
 --TASK 1
-(select maker, speed from pc 
+(select maker, speed from laptop 
 JOIN product
-ON pc.model = product.model
+ON laptop.model = product.model
 where hd >= 9)
 
 
@@ -34,3 +34,48 @@ except
 
 (SELECT maker FROM product
 where product.type = 'pc')
+
+
+--TASK 4
+(SELECT distinct pc1.hd FROM pc as pc1, pc as pc2
+WHERE pc1.code != pc2.code AND pc1.hd = pc2.hd)--TASK 1
+(select maker, speed from laptop 
+JOIN product
+ON laptop.model = product.model
+where hd >= 9)
+
+
+--TASK 2
+(SELECT product.model, price FROM product
+JOIN pc
+ON pc.model = product.model
+where maker = 'B')
+
+UNION
+
+(SELECT product.model, price FROM product
+JOIN printer
+ON printer.model = product.model
+where maker = 'B')
+
+UNION
+
+(SELECT product.model, price FROM product
+JOIN laptop
+ON laptop.model = product.model
+where maker = 'B')
+
+
+--TASK 3
+(SELECT maker FROM product
+where product.type = 'Laptop')
+
+except
+
+(SELECT maker FROM product
+where product.type = 'pc')
+
+
+--TASK 4
+(SELECT distinct pc1.hd FROM pc as pc1, pc as pc2
+WHERE pc1.code != pc2.code AND pc1.hd = pc2.hd)
